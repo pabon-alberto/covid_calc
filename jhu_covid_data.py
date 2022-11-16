40d0232b29666b89ac16acf27095b0f92d605875
Alberto Pabon <pabon.alberto23@gmail.com>
2022-11-15 22:37:12 -0400

    jhu data is not being updated, start attempt on graphing nytimes data

--- jhu_covid_data.py
+++ jhu_covid_data.py
@@ -1,15 +1,17 @@
 import time
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 url = 'https://raw.githubusercontent.com/govex/COVID-19/master/data_tables/testing_data/time_series_covid19_US.csv'
 jh_df = pd.read_csv(url, index_col = 0)
-pr_df = jh_df[jh_df['state'].str.contains('PR')].tail(7)
+pr_df = jh_df[jh_df['state'].str.contains('PR')].tail(7) #Finds last seven days of data from Puerto Rico.
 
+print(pr_df.iloc[6])
 #For finding important columns
-#print(pr_df[['cases_conf_probable','cases_confirmed','cases_probable', 'tests_viral_positive',
-#'tests_viral_negative','tests_viral_total', 'tests_antigen_positive','tests_antigen_total',
-#'people_viral_positive']]) #Prints only PR stats
+# print(pr_df[['cases_conf_probable','cases_confirmed','cases_probable', 'tests_viral_positive',
+# 'tests_viral_negative','tests_viral_total', 'tests_antigen_positive','tests_antigen_total',
+# 'people_viral_positive']]) #Prints only PR stats
 day1 = pr_df.iloc[0] #Stats from a week ago
 yesterday = pr_df.iloc[6] #Yesterday Stats
 
@@ -32,19 +34,23 @@ avg_rate = str((pcrpos_rate+apos_rate)/2)
 #Source for how to calculate positivity rate:
 #https://www.coronavirus.in.gov/map/positive-test-rate.pdf
 
-print('\nStats for the last seven days:\n')
-#Weekly completed PCR Tests or other approved nucleic acid amplification test (NAAT).
-print('PCR Tests: ' + str(y_pcrtests))
-#Weekly PCR Tests or other approved nucleic acid amplification test (NAAT) that resulted positive.
-print('PCR Tests Resulted Positive: '+ str(y_pcrpos))
-#Positivity rate in PCR Tests or other approved nucleic acid amplification test (NAAT).
-print('Positivity Rate with PCR Tests: ' + str(pcrpos_rate) + '%\n')
 
-#Weekly completed Antigen Tests.
-print('Antigen Tests: ' + str(y_atests))
-#Weekly Antigen Tests that resulted positive.
-print('Antigen Tests Resulted Positive: ' + str(y_apos))
-#Positivity rate in Antigen Tests.
-print('Positivity Rate with Antigen Tests: ' + str(apos_rate) + '%\n')
-#Average Positivity Rate
-print('\nAverage Positivity rate: ' + avg_rate + '%\n')
+# print('\nStats for the last seven days:\n')
+# #Weekly completed PCR Tests or other approved nucleic acid amplification test (NAAT).
+# print('PCR Tests: ' + str(y_pcrtests))
+# #Weekly PCR Tests or other approved nucleic acid amplification test (NAAT) that resulted positive.
+# print('PCR Tests Resulted Positive: '+ str(y_pcrpos))
+# #Positivity rate in PCR Tests or other approved nucleic acid amplification test (NAAT).
+# print('Positivity Rate with PCR Tests: ' + str(pcrpos_rate) + '%\n')
+
+# #Weekly completed Antigen Tests.
+# print('Antigen Tests: ' + str(y_atests))
+# #Weekly Antigen Tests that resulted positive.
+# print('Antigen Tests Resulted Positive: ' + str(y_apos))
+# #Positivity rate in Antigen Tests.
+# print('Positivity Rate with Antigen Tests: ' + str(apos_rate) + '%\n')
+# #Average Positivity Rate
+# print('\nAverage Positivity rate: ' + avg_rate + '%\n')
+
+# plt.plot(y_atests, y_apos)
+# plt.show()
